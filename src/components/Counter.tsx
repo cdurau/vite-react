b612b148fe9f8e2990f1518c1829600d03046a08
cdurau <3326546+cdurau@users.noreply.github.com>
2024-03-11 11:25:32 +0100

    Add Function to Clear Counter Component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,5 @@
-import {useEffect, useState} from 'react';
-import {  Grid } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Grid } from '@mui/material';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -7,6 +7,7 @@ import CardContent from '@mui/material/CardContent';
 import AddCircleOutlineSharpIcon from '@mui/icons-material/AddCircleOutlineSharp';
 import RemoveSharpIcon from '@mui/icons-material/RemoveSharp';
 import Typography from '@mui/material/Typography';
+import { DeleteOutline } from '@mui/icons-material';
 
 function Counter() {
     const storedCounter = JSON.parse(localStorage.getItem('counter') || '0')
@@ -36,7 +37,7 @@ function Counter() {
             alignItems="center"
             justifyContent="center"
             sx={{ minHeight: '100vh', minWidth: '100vw' }}>
-            <Card sx={{ minHeight: '100dvh', minWidth: '100vw', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+            <Card sx={{ minHeight: '100dvh', minWidth: '100vw', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
                 <Typography fontSize={120} gutterBottom variant="h2" component="div" textAlign="center" marginTop='15rem'>
                     {counter}
                 </Typography>
@@ -47,6 +48,7 @@ function Counter() {
                 </CardContent>
                 <CardActions sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
                     <Button size="small" sx={{ padding: '20px' }} onClick={() => changeCounter(-1)}><RemoveSharpIcon fontSize='large' /></Button>
+                    <Button size="small" sx={{ color: "tomato", padding: '20px' }} onClick={() => setCounter(0)}><DeleteOutline fontSize='large' /></Button>
                     <Button size="small" sx={{ padding: '20px' }} onClick={() => changeCounter(1)}><AddCircleOutlineSharpIcon fontSize='large' /></Button>
                 </CardActions>
             </Card>
